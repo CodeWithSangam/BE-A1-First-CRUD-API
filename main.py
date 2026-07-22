eed5a012a660874acd9e9d9a3f0f2b5a68326a6d
CodeWithSangam <gsangam781@gmail.com>
2026-07-22 13:32:22 +0530

    Stage 1: root and health endpoints

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ app = FastAPI()
 
 
 # Stage 0 : Hello Server
-@app.get("/")
-async def root(status_code=200):
-    return {'message':'Hello Server'}   
+# @app.get('/')
+# async def root(status_code=200):
+#     return {'message':'Hello Server'}   
 
+# Stage 1: root and health endpoints
+
+@app.get('/')
+async def root():
+    return { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }
+
+@app.get('/health')
+async def health_check():
+    return{'status':'ok'}
