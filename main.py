0c8801a4650feb65a667904c6799e2b489fdc7dc
CodeWithSangam <gsangam781@gmail.com>
2026-07-22 15:46:56 +0530

    Stage 2: read endpoints with 404

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
+list_of_obj = [
+     { 'id': 1, 'title': 'Buy groceries', 'done': False },
+
+     { 'id': 2, 'title': 'Walk the dog', 'done': True },
+
+     { 'id': 3, 'title': 'Read a book', 'done': False }
+
+]
 # Stage 0 : Hello Server
 # @app.get('/')
 # async def root(status_code=200):
@@ -19,3 +27,16 @@ async def root():
 @app.get('/health')
 async def health_check():
     return{'status':'ok'}
+
+# Stage 2: read endpoints with 404
+@app.get('/tasks')
+async def tasks():
+    return list_of_obj
+
+@app.get('/tasks/{id}')
+async def read_item(id:int):
+    for items in list_of_obj:
+        if items['id'] == id:
+            return items
+    return HTTPException(status_code=404, detail=f"Task {id} not found")
+
